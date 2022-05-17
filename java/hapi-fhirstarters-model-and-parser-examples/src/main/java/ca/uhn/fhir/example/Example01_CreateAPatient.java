9b308da4e570f7bbb1b4a27fc10aee7f6a4728a4
Makhate Makhate <makhatemakhate@gmail.com>
2022-05-17 12:05:06 +0300

    will check this out later

--- java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example01_CreateAPatient.java
+++ java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example01_CreateAPatient.java
@@ -1,25 +1,32 @@
 package ca.uhn.fhir.example;
 
-import org.hl7.fhir.dstu3.model.HumanName;
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.dstu3.model.Identifier;
-import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.r4.model.Patient;
 
 public class Example01_CreateAPatient {
    public static void main(String[] theArgs) {
 
       // Create a resource instance
       Patient pat = new Patient();
+      pat.setId("theId");
 
-      // Add a "name" element
-      HumanName name = pat.addName();
-      name.setFamily("Simpson").addGiven("Homer").addGiven("J");
+      FhirContext fhirContext = FhirContext.forR4();
+      System.out.println(fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(pat));
 
-      // Add an "identifier" element
-      Identifier identifier = pat.addIdentifier();
-      identifier.setSystem("http://acme.org/MRNs").setValue("7000135");
+      // // Add a "name" element
+      // HumanName name = pat.addName();
+      // name.setFamily("Simpson").addGiven("Homer").addGiven("J");
 
-      // Model is designed to be chained
-      pat.addIdentifier().setSystem("http://acme.org/MRNs").setValue("12345");
+      // // Add an "identifier" element
+      // Identifier identifier = pat.addIdentifier();
+      // identifier.setSystem("http://acme.org/MRNs").setValue("7000135");
+
+      // // Model is designed to be chained
+      // pat.addIdentifier().setSystem("http://acme.org/MRNs").setValue("12345");
+
+
 
    }
 }
