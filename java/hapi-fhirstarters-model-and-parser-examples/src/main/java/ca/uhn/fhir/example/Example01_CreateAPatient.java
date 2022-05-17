5a7474ef9480a647aa559d45d9798ee29d0761bc
Makhate Makhate <makhatemakhate@gmail.com>
2022-05-17 12:46:39 +0300

    how to add name and surname to resource of type Patient

--- java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example01_CreateAPatient.java
+++ java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example01_CreateAPatient.java
@@ -5,16 +5,43 @@ import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.dstu3.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example01_CreateAPatient {
    public static void main(String[] theArgs) {
 
       // Create a resource instance
-      Patient pat = new Patient();
-      pat.setId("theId");
+      Patient pat = new Patient(); //creating a new resource
+      pat.setId("theId"); // assigning the resource an id
 
       FhirContext fhirContext = FhirContext.forR4();
-      System.out.println(fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(pat));
+//      System.out.println(fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(pat));
 
+      // -----Method 1 | adding identifiers------
+//      pat.addIdentifier();
+//      List<Identifier> ids = new ArrayList<>();
+//      Identifier id  = new Identifier();
+//      id.setSystem("http/example-system");
+//      id.setValue("AA2Test");
+//
+//      Identifier id2 = new Identifier();
+//      id2.setSystem("MOHTest");
+//      id2.setValue("MOHTest");
+//      ids.add(id);
+//      ids.add(id2);
+//      pat.setIdentifier(ids);
+//      pat.setIde();
+      // ---------------------------------
+
+      // -----Method 2 | adding identifiers------
+      pat.addIdentifier().setValue("AA2Test").setSystem("AA2Test");
+      pat.addIdentifier().setValue("MOHTest").setSystem("MOHTest");
+      pat.addName().addGiven("first given name").addGiven("second given name").setFamily("your surname");
+      // ----------------------------------------
+      System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+      System.out.println(fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(pat));
+      System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
       // // Add a "name" element
       // HumanName name = pat.addName();
       // name.setFamily("Simpson").addGiven("Homer").addGiven("J");
@@ -25,8 +52,6 @@ public class Example01_CreateAPatient {
 
       // // Model is designed to be chained
       // pat.addIdentifier().setSystem("http://acme.org/MRNs").setValue("12345");
-
-
 
    }
 }
